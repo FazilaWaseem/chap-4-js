3e50283874200f2969291f272f68eb00e4df5b1c
Fazila Waseem <fazilawaseem829@gmail.com>
2024-08-15 18:38:48 +0500

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+alert("Rules of JS Variables");
+var heading = "<b>Rules for naming JS variables</b>" + "</br>";
+document.write(heading);
+
+var variableNames = " numbers, $, and _";
+document.write("<br>Variable names can only contain," + variableNames + ".For example $my_1stVariable  </br>"); 
+var letter_doller_ = " letter, $, or _";
+document.write("Variables must begin with a" + letter_doller_ + ".For example $name, _name or name  </br>");
+var sensitive = " Sensitive";
+document.write("Variable names are case" + sensitive + "<br/>");
+var keywords = "keywords";
+document.write("Variable names should not be JS " + keywords + "<br/>");
